27659b058fcb11db4974284fe5cda23bf49b92df
Matt Shore <mazzamds@gmail.com>
2021-05-27 23:59:43 +0100

    Efficiency improvement.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-function changeDisplay(type1, type2, type3, type4) {
+function changeDisplay(type) {
     document.querySelectorAll('nav p').forEach(function (element) {
         element.style.color = 'white';
     });
-    document.querySelector('#welcome').style.display = 'none';
-    document.querySelector('#' + type1).style.display = 'block';
-    document.querySelector('#' + type2).style.display = 'none';
-    document.querySelector('#' + type3).style.display = 'none';
-    try {
-        document.querySelector('#' + type4).style.display = 'none';
-    } catch (e) { }
+
+    // We hide everything except the id specified in the function parameter.
+    let elements = ['#welcome', '#cvArticle', '#projectsArticle', '#accountsArticle', '#ibmProject']
+    for (let i = 0; i < elements.length; i++) {
+        elements[i] === type ? document.querySelector(elements[i]).style.display = 'block' : document.querySelector(elements[i]).style.display = 'none';
+    } 
+ 
 };
 
 document.querySelector('#cvNav').addEventListener('click', function () {
-    changeDisplay('cvArticle', 'projectsArticle', 'accountsArticle');
+    changeDisplay('#cvArticle');
     document.querySelector('#cvNav').style.color = 'gold';
 });
 
 document.querySelector('#projectsNav').addEventListener('click', function () {
-    changeDisplay('projectsArticle', 'cvArticle', 'accountsArticle');
+    changeDisplay('#projectsArticle');
     document.querySelector('#projectsNav').style.color = 'gold';
 });
 
-// CV
+
 
 // PROJECTS
 document.querySelector('#ibm').addEventListener('click', function () {
@@ -32,7 +32,7 @@ document.querySelector('#ibm').addEventListener('click', function () {
 // ARTICLES
 
 document.querySelector('#accountsNav').addEventListener('click', function () {
-    changeDisplay('accountsArticle', 'cvArticle', 'projectsArticle');
+    changeDisplay('#accountsArticle');
     document.querySelector('#accountsNav').style.color = 'gold';
     document.querySelectorAll('.articleSelect')[0].addEventListener('click', function () {
         document.querySelector('#litReviewOne').style.display = 'block';
